028247b34f565c50fe49032def4f1d5368ca01df
federpmatc <61851250+federpmatc@users.noreply.github.com>
2022-02-01 11:16:17 -0600

    Add files via upload

---
+++ CreateUsers.ps1
@@ -0,0 +1,32 @@
+﻿#The following script can be used to create bulk users
+#Create 20 Users.ps1
+#11/9/2021
+
+#$DomainName set to your domain
+$domainName = "ITNET.pri"
+
+#$Path should be set to the distinguished name of the OU where users will be created
+#The following will show Distinguished Names for all OUs
+Get-ADOrganizationalUnit -Filter * | select-object name, distinguishedname
+
+$path = "OU=Student,DC=ITNET,DC=pri"
+
+#$total should be set to how many users you want to create
+$total=100
+
+#The following block of code will get executed multiple times (or whatever the value of $total is set to.
+1..$total |foreach { 
+$userName = "TestStudent$_"
+Write-Host "Creating user $userName@$domainName.  User $_ of $total" 
+
+New-ADUser -AccountPassword (ConvertTo-SecureString "Password01" -AsPlainText -Force) `
+-Name "$userName" `
+-Enabled $true `
+-Path $path `
+-SamAccountName "$userName" `
+-UserPrincipalName ($userName + "@" + $domainName)
+}
+
+Get-ADUser -Filter * -SearchBase $path
+
+
---
+++ EmailAddressSolution.ps1
@@ -0,0 +1,15 @@
+﻿#Here are the commands used to complete the Email Address Assignment.
+#Don't run them all at one time, you will need to selectively run them
+
+New-ADOrganizationalUnit -name Student -path "DC=ITNET, DC=pri"
+Get-ADOrganizationalUnit -Filter *
+Get-ADUser -Filter {name -like "Test*"} -Properties EmailAddress | Export-Csv "C:\users.csv"
+Get-Content C:\users.csv 
+Import-Csv C:\users2.csv | Out-GridView
+
+import-csv C:\users2.csv | ForEach-Object {
+$username = $_.name
+$EmailAddress = $_.EmailAddress
+#Write-Host $username, $EmaiAddress
+Get-ADUser -Filter {name -eq $username} | Set-ADUser -EmailAddress $EmailAddress
+}
